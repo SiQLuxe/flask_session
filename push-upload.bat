0317cc3dc92a5844a37c77e5fce54dcaa9f0998c
Luxe <csq@qstrategy.cn>
2016-05-17 22:41:48 +0800

    udpate setup

--- push-upload.bat
+++ push-upload.bat
@@ -4,4 +4,4 @@ git push origin master
 git tag 0.3.0 -m "put on PyPI"
 git push --tags origin master
 
-# http://akagi201.org/blog/how-to-submit-package-to-pypi/
+echo http://akagi201.org/blog/how-to-submit-package-to-pypi/
