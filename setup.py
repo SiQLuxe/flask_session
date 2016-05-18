5283388e0a2b9e1ccde33d2c355970d9ba9db33d
Luxe <csq@qstrategy.cn>
2016-05-18 11:05:16 +0800

    udpate setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 
 setup(
-    name='Flask-CNSession',
+    name='Flask-QSSession',
     version='0.3.0',
     url='https://github.com/SiQLuxe/flask_session',
     download_url='https://github.com/SiQLuxe/flask_session/tree/0.3.0',
